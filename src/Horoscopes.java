d606a0940d458ebfb650313736a821930bf37523
ashlynaguirre <league@Mac-mini-3.local>
2016-03-09 18:56:26 -0800

    3/9/16

--- src/Horoscopes.java
+++ src/Horoscopes.java
@@ -5,4 +5,8 @@ String answer = JOptionPane.showInputDialog("what is your zodiac sing");
 if (answer.equalsIgnoreCase("aries")){
 	JOptionPane.showMessageDialog(null, "You are known for being mean");
 }
+else if (answer.equalsIgnoreCase("Taurus"));{
+	JOptionPane.showMessageDialog(null, "You are known for being stubborn");
+}
+}
 }
